fac6c9aab1e83652b8451ef4320d6319ef785ddc
lele94218 <terry.trxue@gmail.com>
2017-01-13 20:04:27 -0500

    init project

---
+++ assembler/assembler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  assembler
+//
+//  Created by Taoran Xue on 1/13/17.
+//  Copyright © 2017 Taoran Xue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
