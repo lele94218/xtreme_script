2c867fb9452fdcdf9b3f23cf5058e9ad1d959b09
lele94218 <terry.trxue@gmail.com>
2017-02-13 01:29:51 -0500

    operand process

--- virtualmachine/main.cpp
+++ virtualmachine/main.cpp
@@ -85,84 +85,84 @@ typedef struct _Value
 {
     int iType;
     union
-	{
-		int iIntLiteral;
-		float fFloatLiteral;
-		char * pstrStringLiteral;
-		int iStackIndex;
-		int iInstrIndex;
+    {
+        int iIntLiteral;
+        float fFloatLiteral;
+        char * pstrStringLiteral;
+        int iStackIndex;
+        int iInstrIndex;
         int iFuncIndex;
-		int iHostAPICallIndex;
-		int iReg;
+        int iHostAPICallIndex;
+        int iReg;
     };
-	int iOffsetIndex;
+    int iOffsetIndex;
 }
 Value;
 
 /* An instruction */
 typedef struct _Instr
 {
-	int iOpcode;
-	int iOpCount;
-	Value * pOpList;
+    int iOpcode;
+    int iOpCount;
+    Value * pOpList;
 }
 Instr;
 
 /* An instruction stream */
 typedef struct _InstrStream
 {
-	Instr * pInstr;
-	int iSize;
-	int iCurrInstr;
+    Instr * pInstr;
+    int iSize;
+    int iCurrInstr;
 }
 InstrStream;
 
 /* A runtime stack */
 typedef struct _RuntimeStack
 {
-	Value * pElmnts;
-	int iSize;
-	int iTopIndex;
-	int iFrameIndex;
+    Value * pElmnts;
+    int iSize;
+    int iTopIndex;
+    int iFrameIndex;
 }
 RuntimeStack;
 
 /* Function table element */
 typedef struct _Func
 {
-	int iEntryPoint;
-	int iParamCount;
-	int iLocalDataSize;
-	int iStackFrameSize;
+    int iEntryPoint;
+    int iParamCount;
+    int iLocalDataSize;
+    int iStackFrameSize;
 }
 Func;
 
 /* A host API call table */
 typedef struct _HostAPICallTable
 {
-	char ** ppstrCalls;
-	int iSize;
+    char ** ppstrCalls;
+    int iSize;
 }
 HostAPICallTable;
 
 /* Encapsulates a full script */
 typedef struct _Script
 {
-	int iGlobalDataSize;
-	int iIsMainFuncPresent;
-	int iMainFuncIndex;
-	int iIsPaused;
-	int iPauseEndTime;
-
-	/* Register file */
-	Value _RetVal;
-
-	/* Script data */
-	InstrStream InstrStream;
-	
-	RuntimeStack Stack;
-	Func * pFuncTable;
-	HostAPICallTable HostAPICallTable;	
+    int iGlobalDataSize;
+    int iIsMainFuncPresent;
+    int iMainFuncIndex;
+    int iIsPaused;
+    int iPauseEndTime;
+    
+    /* Register file */
+    Value _RetVal;
+    
+    /* Script data */
+    InstrStream InstrStream;
+    
+    RuntimeStack Stack;
+    Func * pFuncTable;
+    HostAPICallTable HostAPICallTable;
 }
 Script;
 
@@ -171,17 +171,25 @@ Script g_Script;
 
 /* Function declarations */
 int LoadScript(char *);
+int GetOpType(int);
+int ResloveOpStackIndex(int);
+Value ResolveOpValue(int);
+Value GetStackValue(int);
+int ResolveOpAsInt(int);
+float ResolveOpAsFloat(int);
+char * ResolveOpAsString(int);
+char * CoerceValueToString(Value);
 
 /* Function implements */
 int LoadScript(char * pstrFilename)
 {
-	FILE * pScriptFile;
+    FILE * pScriptFile;
     if (!(pScriptFile = fopen(pstrFilename, "rb")))
         return LOAD_ERROR_FILE_IO;
-	
-	char * pstrIDString;
-	pstrIDString = (char *) malloc (5);
-
+    
+    char * pstrIDString;
+    pstrIDString = (char *) malloc (5);
+    
     fread(pstrIDString, 4, 1, pScriptFile);
     pstrIDString[strlen(XSE_ID_STRING)] = '\0';
     
@@ -287,9 +295,233 @@ int LoadScript(char * pstrFilename)
             
             ppstrStringTable[iCurrStringIndex] = pstrCurrString;
         }
+        
+        /* Run through each operand in the instruction stream and assign copies of string */
+        for (int iCurrInstrIndex = 0; iCurrInstrIndex < g_Script.InstrStream.iSize; ++ iCurrInstrIndex)
+        {
+            int iOpCount = g_Script.InstrStream.pInstr[iCurrInstrIndex].iOpCount;
+            Value * pOpList = g_Script.InstrStream.pInstr[iCurrInstrIndex].pOpList;
+            
+            for (int iCurrOpIndex = 0; iCurrOpIndex < iOpCount; ++ iCurrOpIndex)
+            {
+                if (pOpList[iCurrOpIndex].iType == OP_TYPE_STRING)
+                {
+                    int iStringIndex = pOpList[iCurrOpIndex].iIntLiteral;
+                    char * pstrStringCopy = (char *) malloc(strlen(ppstrStringTable[iStringIndex]) + 1);
+                    strcpy(pstrStringCopy, ppstrStringTable[iStringIndex]);
+                    pOpList[iCurrOpIndex].pstrStringLiteral = pstrStringCopy;
+                }
+            }
+        }
+        
+        /* Free the orginal strings */
+        for (int iCurrStringIndex = 0; iCurrStringIndex < iStringTableSize; ++ iCurrStringIndex)
+        {
+            free(ppstrStringTable[iCurrStringIndex]);
+        }
+        free(ppstrStringTable);
+        
+        int iFuncTableSize;
+        fread(&iFuncTableSize, 4, 1, pScriptFile);
+        g_Script.pFuncTable = (Func *) malloc(iFuncTableSize * sizeof(Func));
+        
+        for (int iCurrFuncIndex = 0; iCurrFuncIndex < iFuncTableSize; ++ iCurrFuncIndex)
+        {
+            int iEntryPoint;
+            fread(&iEntryPoint, 4, 1, pScriptFile);
+            
+            int iParamCount = 0;
+            fread(&iParamCount, 1, 1, pScriptFile);
+            
+            int iLocalDataSize;
+            fread(&iLocalDataSize, 4, 1, pScriptFile);
+            
+            int iStackFrameSize = iParamCount + 1 + iLocalDataSize;
+            
+            g_Script.pFuncTable[iCurrFuncIndex].iEntryPoint = iEntryPoint;
+            g_Script.pFuncTable[iCurrFuncIndex].iParamCount = iParamCount;
+            g_Script.pFuncTable[iCurrFuncIndex].iLocalDataSize = iLocalDataSize;
+            g_Script.pFuncTable[iCurrFuncIndex].iStackFrameSize = iStackFrameSize;
+        }
+        
+        /* Read the host API call table */
+        
+        fread(&g_Script.HostAPICallTable.iSize, 4, 1, pScriptFile);
+        g_Script.HostAPICallTable.ppstrCalls = (char **) malloc(g_Script.HostAPICallTable.iSize * sizeof(char *));
+        
+        for (int iCurrCallIndex = 0; iCurrCallIndex < g_Script.HostAPICallTable.iSize; ++ iCurrCallIndex)
+        {
+            int iCallLength = 0;
+            fread(&iCallLength, 1, 1, pScriptFile);
+            
+            char * pstrCurrCall = (char *) malloc(iCallLength + 1);
+            
+            fread(pstrCurrCall, iCallLength, 1, pScriptFile);
+            pstrCurrCall[iCallLength] = '\0';
+            
+            g_Script.HostAPICallTable.ppstrCalls[iCurrCallIndex] = pstrCurrCall;
+        }
+        
+        fclose ( pScriptFile );
+        
+        /* Print some information about the script */
+        
+        printf ( "%s loaded successfully!\n", pstrFilename );
+        printf ( "\n" );
+        printf ( "  Format Version: %d.%d\n", iMajorVersion, iMinorVersion );
+        printf ( "      Stack Size: %d\n", g_Script.Stack.iSize );
+        printf ( "Global Data Size: %d\n", g_Script.iGlobalDataSize );
+        printf ( "       Functions: %d\n", iFuncTableSize );
+        
+        printf ( "_Main () Present: " );
+        if (g_Script.iIsMainFuncPresent)
+        {
+            printf ( "Yes (Index %d)", g_Script.iMainFuncIndex );
+        }
+        else
+        {
+            printf ( "No" );
+        }
+        printf ( "\n" );
+        
+        printf ( "  Host API Calls: %d\n", g_Script.HostAPICallTable.iSize );
+        printf ( "    Instructions: %d\n", g_Script.InstrStream.iSize );
+        printf ( " String Literals: %d\n", iStringTableSize );
+        
+        printf ( "\n" );
+        
     }
     
     return LOAD_OK;
+}
+
+/* Coerces a Value structure from it's current type to a string value. */
+char * CoerceValueToString(Value Val)
+{
+    char * pstrCoercion;
+    if (Val.iType != OP_TYPE_STRING)
+        pstrCoercion = (char *) malloc(MAX_COERCION_STRING_SIZE + 1);
+    
+    switch (Val.iType) {
+        case OP_TYPE_INT:
+            sprintf(pstrCoercion, "%d", Val.iIntLiteral);
+            return pstrCoercion;
+        case OP_TYPE_FLOAT:
+            sprintf(pstrCoercion, "%f", Val.fFloatLiteral);
+            return pstrCoercion;
+        case OP_TYPE_STRING:
+            return Val.pstrStringLiteral;
+        default:
+            return NULL;
+    }
+}
+
+/* Coerces a Value structure from it's current type to an integer value. */
+int CoerceValueToInt(Value Val)
+{
+    switch (Val.iType) {
+        case OP_TYPE_INT:
+            return Val.iIntLiteral;
+        case OP_TYPE_FLOAT:
+            return (int) Val.fFloatLiteral;
+        case OP_TYPE_STRING:
+            return atoi(Val.pstrStringLiteral);
+        default:
+            return 0;
+    }
+}
+
+float CoerceValueToFloat(Value Val)
+{
+    switch (Val.iType) {
+        case OP_TYPE_INT:
+            return (float) Val.iIntLiteral;
+        case OP_TYPE_FLOAT:
+            return Val.fFloatLiteral;
+        case OP_TYPE_STRING:
+            return atof(Val.pstrStringLiteral);
+        default:
+            return 0;
+    }
+}
+
+int GetOpType(int iOpIndex)
+{
+    int iCurrInstr = g_Script.InstrStream.iCurrInstr;
+    
+    return g_Script.InstrStream.pInstr[iCurrInstr].pOpList[iOpIndex].iType;
+}
+
+
+int ResolveOpAsInt(int iOpIndex)
+{
+    Value OpValue = ResolveOpValue(iOpIndex);
+    int iInt = CoerceValueToInt(OpValue);
+    return iInt;
+}
+
+float ResolveOpAsFloat(int iOpIndex)
+{
+    Value OpValue = ResolveOpValue(iOpIndex);
+    float fFloat = CoerceValueToFloat(OpValue);
+    return fFloat;
+}
+
+char * ResolveOpAsString(int iOpIndex)
+{
+    Value OpValue = ResolveOpValue(iOpIndex);
+    char * pstrString = CoerceValueToString(OpValue);
+    return pstrString;
+}
+
+int ResloveOpStackIndex(int iOpIndex)
+{
+    return 0;
+}
+
+/* Resolves an operand and returns a pointer to it's Value structure */
+Value * ResolveOpPntr(int iOpIndex)
+{
+    int iIndirMethod = GetOpType(iOpIndex);
+    
+    switch (iIndirMethod) {
+        case OP_TYPE_REG:
+            return &g_Script._RetVal;
+        case OP_TYPE_ABS_STACK_INDEX:
+        case OP_TYPE_REL_STACK_INDEX:
+        {
+            int iStackIndex = ResloveOpStackIndex(iOpIndex);
+            return &g_Script.Stack.pElmnts[ResloveOpStackIndex(iStackIndex)];
+        }
+        default:
+            return NULL;
+    }
+}
+
+/* Resolves an operand and returns a pointer to it's Value structure */
+Value ResolveOpValue(int iOpIndex)
+{
+    int iCurrInstr = g_Script.InstrStream.iCurrInstr;
+    Value OpValue = g_Script.InstrStream.pInstr[iCurrInstr].pOpList[iOpIndex];
+    
+    switch (OpValue.iType) {
+        case OP_TYPE_ABS_STACK_INDEX:
+        case OP_TYPE_REL_STACK_INDEX:
+        {
+            int iAbsIndex = ResloveOpStackIndex(iOpIndex);
+            return GetStackValue(iAbsIndex);
+        }
+        case OP_TYPE_REG:
+            return g_Script._RetVal;
+        default:
+            return OpValue;
+    }
+}
+
+Value GetStackValue(int iIndex)
+{
+    Value v;
+    return v;
 }
 
 int main()
