799089169ef62e32ebc66cf9c39e15f335ce9524
lele94218 <terry.trxue@gmail.com>
2017-05-12 13:35:38 -0400

    add string

--- MLCC/src/c.h
+++ MLCC/src/c.h
@@ -6,6 +6,9 @@
 
 // #define PURIFY
 
+#define NEW(p, a) ((p) = allocate(sizeof *(p), (a)))
+#define NEW0(p, a) memset(NEW((p), (a)), 0, sizeof *(p))
+
 #define NELEMS(a) ((int)(sizeof(a) / sizeof((a)[0])))
 #undef roundup
 #define roundup(x, n) (((x) + ((n) - 1)) & (~((n) - 1)))
@@ -14,3 +17,16 @@
 extern void *allocate(unsigned long n, unsigned a);
 extern void deallocate(unsigned a);
 extern void *newarray(unsigned long m, unsigned long n, unsigned a);
+
+
+extern char *string(const char *str);
+extern char *stringn(const char *str, int len);
+extern char *stringd(long n);
+
+enum
+{
+    /* alloc blocks */
+	PERM = 0, // permanent data
+	FUNC,
+	STMT
+};
--- MLCC/src/main.c
+++ MLCC/src/main.c
@@ -1,9 +1,12 @@
 #include "c.h"
 
+
 int main(int argc, char * argv[])
 {
     int * a = newarray(10, sizeof(int), 1);
-    printf("%lu\n", sizeof(char));
+    printf("%lu %lu\n", sizeof(unsigned), sizeof(long));
     printf("%p %p %p\n", a, a + 1, &a[1]);
     printf("hello\n");
+   
+    
 }
