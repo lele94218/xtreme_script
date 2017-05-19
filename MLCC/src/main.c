fadeec27956341f1da6ea01b8c49442ec0e69f8f
lele94218 <terry.trxue@gmail.com>
2017-05-19 15:08:52 -0400

    strings

--- MLCC/src/main.c
+++ MLCC/src/main.c
@@ -2,11 +2,8 @@
 
 
 int main(int argc, char * argv[])
-{
-    int * a = newarray(10, sizeof(int), 1);
-    printf("%lu %lu\n", sizeof(unsigned), sizeof(long));
-    printf("%p %p %p\n", a, a + 1, &a[1]);
-    printf("hello\n");
-   
-    
+{  
+    char * s = string("abc");
+    char * t = string("abc");
+    printf("%p %p\n", s, t);
 }
